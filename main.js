4394dce28a4fa0857b627441a48135d620d20fda
Daniel Ronan <danielconnorronan@gmail.com>
2020-12-09 10:18:21 +0000

    format code

--- main.js
+++ main.js
@@ -1,52 +1,53 @@
-  const { newBookForm } = document.forms;
-  const tbody = document.querySelector('#tbody');
-  const tr = document.createElement('tr');
-  const title = document.createElement('td');
-  const author = document.createElement('td');
-  const pages = document.createElement('td');
-  const read = document.createElement('td');
+const tbody = document.querySelector("#tbody");
+const tr = document.createElement("tr");
+const title = document.createElement("td");
+const author = document.createElement("td");
+const pages = document.createElement("td");
+const read = document.createElement("td");
 
-  const Book = (title, author, pages, read) => ({
-    title, author, pages, read
-  });
+const Book = (title, author, pages, read) => ({
+  title,
+  author,
+  pages,
+  read,
+});
 
 const myLibrary = [];
 const addBookToLibrary = (book) => {
   myLibrary.push(book);
 };
 
-
 const deleteBook = (book, tr) => {
-  const createBtn = document.createElement('button');
+  const createBtn = document.createElement("button");
 
-  createBtn.addEventListener('click', () => {
+  createBtn.addEventListener("click", () => {
     tr.parentNode.removeChild(tr);
     if (myLibrary.indexOf(book) !== -1) {
       myLibrary.splice(myLibrary.indexOf(book), 1);
     }
   });
 
-  createBtn.textContent = 'Delete';
-  createBtn.setAttribute('class', 'btn btn-danger mr-2 ml-3');
+  createBtn.textContent = "Delete";
+  createBtn.setAttribute("class", "btn btn-danger mr-2 ml-3");
   tr.appendChild(createBtn);
 };
 
 const readStatusToggle = (book, tr) => {
-  const createBtn = document.createElement('button');
+  const createBtn = document.createElement("button");
 
-  createBtn.addEventListener('click', () => {
-    const status = tr.querySelector('td:nth-child(4)');
-    if (book.read === 'Yes') {
-      status.textContent = 'No';
-      book.read = 'No';
+  createBtn.addEventListener("click", () => {
+    const status = tr.querySelector("td:nth-child(4)");
+    if (book.read === "Yes") {
+      status.textContent = "No";
+      book.read = "No";
     } else {
-      status.textContent = 'Yes';
-      book.read = 'Yes';
+      status.textContent = "Yes";
+      book.read = "Yes";
     }
   });
 
-  createBtn.textContent = 'Toggle status';
-  createBtn.setAttribute('class', 'btn btn-success');
+  createBtn.textContent = "Toggle status";
+  createBtn.setAttribute("class", "btn btn-success");
   tr.appendChild(createBtn);
 };
 
@@ -65,16 +66,17 @@ const displayBook = (book) => {
   tbody.appendChild(tr);
 };
 
-document.querySelector('#form-btn').addEventListener('click', () => {
-  newBookForm.style.display = 'block';
+document.querySelector("#form-btn").addEventListener("click", () => {
+  newBookForm.style.display = "block";
 });
 
-document.querySelector('#cancel-link').addEventListener('click', () => {
-  newBookForm.style.display = 'none';
+document.querySelector("#cancel-link").addEventListener("click", () => {
+  newBookForm.style.display = "none";
 });
 
+newBookForm = document.forms;
 
-newBookForm.addEventListener('submit', (e) => {
+newBookForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const title = newBookForm.querySelector('input[name="title"').value;
   const author = newBookForm.querySelector('input[name="author"').value;
@@ -82,9 +84,9 @@ newBookForm.addEventListener('submit', (e) => {
   const possibleReadValues = newBookForm.querySelectorAll('input[name="read"]');
   let read;
   if (possibleReadValues[0].checked) {
-    read = 'Yes';
+    read = "Yes";
   } else {
-    read = 'No';
+    read = "No";
   }
 
   const book = Book(title, author, pages, read);
